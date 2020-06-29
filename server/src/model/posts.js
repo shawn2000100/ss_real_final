ef8c9df6545e26d74080edba9b90466fa9074186
107062318 <karta15025452@gmail.com>
2020-06-29 22:21:29 +0800

    hello

--- server/src/model/posts.js
+++ server/src/model/posts.js
@@ -19,15 +19,6 @@ function list(searchText = '', start) {
     return db.any(sql, [searchText, start]);
 }
 
-<<<<<<< HEAD
-function create(mood, text, title, location, userName) {
-    const sql = `
-        INSERT INTO posts ($<this:name>)
-        VALUES ($<mood>, $<text>, $<title>, $<location>, $<userName>)
-        RETURNING *
-    `;
-    return db.one(sql, {mood, text, title, location, userName});
-=======
 function create(mood, text, title, location, username) {
     console.log(username)
     console.log("Post.js Model");
@@ -37,7 +28,6 @@ function create(mood, text, title, location, username) {
         RETURNING *
     `;
     return db.one(sql, {mood, text, title, location, username});
->>>>>>> 4dd2ba07159823dc38ad81a93eea16f17a0e166a
 }
 
 module.exports = {
--- server/src/routers/posts.js
+++ server/src/routers/posts.js
@@ -20,22 +20,13 @@ router.get('/posts', function(req, res, next) {
 
 // Create
 router.post('/posts', function(req, res, next) {
-<<<<<<< HEAD
-    const {mood, text, title, location, name} = req.body;
-    if (!mood || !text || !title || !location || !name) {
-=======
     const {mood, text, title, location, username} = req.body;
     if (!mood || !text || !title || !location || !username) {
->>>>>>> 4dd2ba07159823dc38ad81a93eea16f17a0e166a
         const err = new Error('Mood and text are required');
         err.status = 400;
         throw err;
     }
-<<<<<<< HEAD
-    postModel.create(mood, text, title, location, name).then(post => {
-=======
     postModel.create(mood, text, title, location, username).then(post => {
->>>>>>> 4dd2ba07159823dc38ad81a93eea16f17a0e166a
         res.json(post);
     }).catch(next);
 });
