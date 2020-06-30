f2e451a54b4a84ca0d078e294644a2b9eb06c7cf
JC <shawn2000100@gmail.com>
2020-06-30 12:01:13 +0800

    rds

--- server/src/server.js
+++ server/src/server.js
@@ -14,13 +14,6 @@ app.use(express.static('dist', {
         res.set('Cache-Control', 'public, s-maxage=86400');
     }
 }));
-// [JC]0512: 這次好像不用設這個，Middleware做過了
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-//     res.header("Access-Control-Allow-Headers", "X-Requested-With, content-type, Authorization");
-//     next();
-//   });
 app.use('/api', postRouter);
 app.use('/api', todoRouter);
 app.get('/*', (req, res) => res.redirect('/'));
